77e8a3d2a513c3937c0aa2c922730b440a721828
Adam Ujvari <adam.ujvari@outlook.com>
2017-11-24 17:56:32 +0100

    -

--- README.md
+++ README.md
@@ -1,19 +1,16 @@
+# lib-indexer
 # Document indexing and retrieval using text and images, supporting literature search in the visualization domain.
 
 Practical Assignment for Digital Libraries ()
 
 Group details:
 - Adam Ujvari, 01232056, adam.ujvari@student.tugraz.at
-- Nikita , , @student.tugraz.at
+- Nikita Lvov, 01639595, lvov@student.tugraz.at
 
 # Description
 
 
 # How to run
-Place your pdf collection into the *Collection* folder. Please make sure there is one layer of subfolders. 
-
-    ex: Collection/subfolder/*.pdf
-To extract the plaintext from all the pdfs, run the _extractor.sh_ script.
 
 
 
---
+++ lib-indexer.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+extract_plaintext () {
+  echo " --- Extract plaintext from PDFs --- ";
+  for sub_folder in Collection/*
+  do
+    mkdir -p plaintext
+    for pdf in $sub_folder/*.pdf
+    do
+      java -jar pdfbox-app-2.0.8.jar ExtractText $pdf
+    done
+    mv $sub_folder/*.txt plaintext/
+  done
+}
+
+build_index () {
+  echo " --- Build Search Index --- ";
+  java org.apache.lucene.demo.IndexFiles -docs plaintext/
+}
+
+search_query () {
+  echo " --- Enter Search Query --- ";
+  java org.apache.lucene.demo.SearchFiles
+}
+
+while true; do
+  clear
+  echo " --- Document indexing and retrieval using text and images. ---";
+  echo " Mode | Description"
+  echo "  1   | Extract PDFs into plaintext";
+  echo "  2   | Build Search Index";
+  echo "  3   | Enter Search Query";
+  echo "  4   | Quit";
+  echo " --- Choose a program operation mode: ";
+  read input
+
+  if [ $input == '1' ]; then
+    extract_plaintext
+
+  elif [ $input == '2' ]; then
+    build_index
+  
+  elif [ $input == '3' ]; then
+    search_query
+
+  elif [ $input == '4' ]; then
+    clear
+    exit 0
+
+  fi
+
+done
+
